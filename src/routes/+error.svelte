b2e58ba0bc99812b0748f7d5bdaed2b9bc957e12
ananduremanan <ananduor@gmail.com>
2024-05-08 17:13:33 +0530

    Production issue fix try #2

--- src/routes/+error.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  import { Meh, Car } from "lucide-svelte";
-</script>
-
-<div class="flex gap-2">
-  <Meh class="icon-rotate" />
-  <div><span class="font-bold">404</span> | Can't Find on our Server</div>
-</div>
