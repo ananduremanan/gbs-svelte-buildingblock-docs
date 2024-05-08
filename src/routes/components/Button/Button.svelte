f5c91963607f398c2522d9649a5f8e44c3caf4d9
ananduremanan <ananduor@gmail.com>
2024-05-08 11:32:23 +0530

    Button Sample Added

---
+++ src/routes/components/Button/Button.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import Button from "@grampro/svelte-block/Button.svelte";
+  import { Bug } from "lucide-svelte";
+</script>
+
+<div class="flex gap-1">
+  <Button>Submit</Button>
+  <Button outline class="hover:bg-orange-500">Submit</Button>
+  <Button class="bg-red-500 hover:bg-red-700">Submit</Button>
+  <Button class="bg-green-500 hover:bg-green-700 flex gap-1"><Bug size="20" />Report Bug</Button>
+</div>
