1d71fc809cff7df3e076ca2367a004b9f009046c
ananduremanan <ananduor@gmail.com>
2024-05-09 16:00:39 +0530

    Select Component Added

---
+++ src/routes/components/Select/Select.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Select from "@grampro/svelte-block/Select.svelte";
+
+  export let countries = [
+    { value: "us", label: "United States" },
+    { value: "fr", label: "France" },
+    { value: "gb", label: "United Kingdom" },
+    { value: "de", label: "Germany" },
+    { value: "jp", label: "Japan" },
+    { value: "au", label: "Australia" },
+    { value: "cn", label: "China" },
+    { value: "in", label: "India" },
+    { value: "mx", label: "Mexico" },
+  ];
+</script>
+
+<div class="relative">
+  <Select items={countries} />
+</div>
