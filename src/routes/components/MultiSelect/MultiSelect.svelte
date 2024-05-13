780f71940c73c3590ed519e81adf5b8af9e1336b
ananduor <ananduor@gmail.com>
2024-05-13 15:09:01 +0530

    Added MultiSelect Preview

---
+++ src/routes/components/MultiSelect/MultiSelect.svelte
@@ -0,0 +1,19 @@
+<script>
+  import MultiSelect from "@grampro/svelte-block/MultiSelect.svelte";
+
+  export let countries = [
+    { value: "us", label: "United States" },
+    { value: "fr", label: "France" },
+    { value: "gb", label: "United Kingdom" },
+    { value: "de", label: "Germany" },
+    { value: "jp", label: "Japan" },
+    { value: "au", label: "Australia" },
+    { value: "cn", label: "China" },
+    { value: "in", label: "India" },
+    { value: "mx", label: "Mexico" },
+  ];
+</script>
+
+<div class="relative">
+  <MultiSelect items={countries} />
+</div>
