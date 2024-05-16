d8c62e714f1bcea327f1f39256413b0b8efceed7
anandhuremanan <ananduor@gmail.com>
2024-05-16 09:12:58 +0530

    TextInput Component Added

---
+++ src/routes/components/TextInput/TextInput.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import TextInput from "@grampro/svelte-block/TextInput.svelte";
+
+  export let OTPFeildClient: boolean = false;
+</script>
+
+<TextInput OTPFeild={OTPFeildClient} />
