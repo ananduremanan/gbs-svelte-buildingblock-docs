2a2327fb0829580d3ae7febdaf4120bb46fd8dbd
anandhuremanan <ananduor@gmail.com>
2024-05-16 09:31:28 +0530

    Report Bug Easter Egg

--- src/routes/components/Button/Button.svelte
+++ src/routes/components/Button/Button.svelte
@@ -1,11 +1,29 @@
 <script lang="ts">
   import Button from "@grampro/svelte-block/Button.svelte";
   import { Bug } from "lucide-svelte";
+
+  let showBug: boolean = false;
+
+  const showBugFunction = () => {
+    showBug = true;
+    setTimeout(() => {
+      showBug = false;
+    }, 2000);
+  };
 </script>
 
 <div class="flex gap-1">
   <Button>Submit</Button>
   <Button outline class="hover:bg-orange-500 dark:text-white">Submit</Button>
   <Button class="bg-red-500 hover:bg-red-700">Submit</Button>
-  <Button class="bg-green-500 hover:bg-green-700 flex gap-1"><Bug size="20" />Report Bug</Button>
+  <Button
+    class="bg-green-500 hover:bg-green-700 flex gap-1"
+    on:click={showBugFunction}><Bug size="20" />Report Bug</Button
+  >
 </div>
+
+{#if showBug}
+  <div>
+    <img src="/bugcss.svg" alt="" class="fixed top-10 md:w-80 md:h-80" />
+  </div>
+{/if}
