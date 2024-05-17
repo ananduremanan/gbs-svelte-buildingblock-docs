be360b0ef2d54e9c27ed0103a839cbfe6b5951f3
anandhuremanan <ananduor@gmail.com>
2024-05-17 10:16:32 +0530

    Refactored UI

---
+++ src/routes/UI/Navbar.svelte
@@ -0,0 +1,5 @@
+<nav class="bg-blue-600 text-white p-4">
+  <div class="container mx-auto">
+    <span class="text-lg font-semibold">Gram Svelte Component Library</span>
+  </div>
+</nav>
---
+++ src/routes/UI/Sidebar.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { onMount } from "svelte";
+
+  let menuItemsArray: any[] = [];
+
+  async function fetchData() {
+    const modules = import.meta.glob("../components/**/*.svx");
+    const menuItems = Object.keys(modules).map(
+      (item) => item.replace("../components/", "").split("/")[0]
+    );
+    menuItemsArray = menuItems;
+  }
+
+  let current_url = "/";
+  $: {
+    current_url = $page.url.pathname.split("/").filter(Boolean).pop() || "/";
+  }
+
+  onMount(fetchData);
+</script>
+
+<div
+  class="h-full border-r overflow-y-auto bg-gray-100 dark:bg-gray-800 dark:text-white p-4 w-1/4 hidden md:block"
+>
+  <ul>
+    <li
+      class={`mt-2 py-1 ${current_url === "/" ? "bg-blue-200 rounded-lg text-blue-500 dark:bg-blue-300" : ""}`}
+    >
+      <a href="/" class="block p-1">Introduction</a>
+    </li>
+    {#each menuItemsArray as menu}
+      <li
+        class={`mt-2 py-1 ${current_url === menu ? "bg-blue-200 text-blue-500 rounded-lg dark:bg-blue-300" : ""}`}
+      >
+        <a href={`/components/${menu}`} class="block p-1">{menu}</a>
+      </li>
+    {/each}
+    <li
+      class={`mt-2 py-1 ${current_url === "Experimental" ? "bg-blue-200 rounded-lg text-blue-500 dark:bg-blue-300" : ""}`}
+    >
+      <a href="/Experimental" class="block p-1">Experimental</a>
+    </li>
+  </ul>
+</div>
