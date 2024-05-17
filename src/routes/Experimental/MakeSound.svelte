8cdccc51e56e351238604487250f71e5b4ed2769
anandhuremanan <ananduor@gmail.com>
2024-05-17 09:22:24 +0530

    icons and style for beatbox kit

--- src/routes/Experimental/MakeSound.svelte
+++ src/routes/Experimental/MakeSound.svelte
@@ -5,10 +5,26 @@
   const playClickSound = MakeSound("/sounds/click.mp3");
 
   const sounds = [
-    { name: "Kick", sound: MakeSound("sounds/beat/kick.mp3") },
-    { name: "Snare", sound: MakeSound("sounds/beat/snare.wav") },
-    { name: "Cymbal", sound: MakeSound("sounds/beat/cymbal.wav") },
-    { name: "Perc", sound: MakeSound("sounds/beat/perc.wav") },
+    {
+      name: "Kick",
+      sound: MakeSound("sounds/beat/kick.mp3"),
+      icon: "/icons/kick.webp",
+    },
+    {
+      name: "Snare",
+      sound: MakeSound("sounds/beat/snare.wav"),
+      icon: "/icons/snare.webp",
+    },
+    {
+      name: "Hi-Hat",
+      sound: MakeSound("sounds/beat/cymbal.wav"),
+      icon: "/icons/cymbal.webp",
+    },
+    {
+      name: "Perc",
+      sound: MakeSound("sounds/beat/perc.wav"),
+      icon: "/icons/tambourine.webp",
+    },
   ];
 
   let clickedIndex: any = null;
@@ -33,12 +49,15 @@
 <Button on:click={playClickSound}>Click to Make Sound</Button>
 
 <div class="mt-4 grid grid-cols-2 gap-4 h-80 w-80">
-  {#each sounds as { name }, index}
+  {#each sounds as { name, icon }, index}
     <Button
       outline
-      class={`${index === clickedIndex ? "border border-blue-500" : ""}w-30 h-15`}
+      class={`${index === clickedIndex ? "border-4 border-red-500" : ""}w-30 h-15 focus:outline-none hover:text-blue-700 flex flex-col items-center justify-center dark:text-white`}
       on:click={() => handleButtonClick(index, true)}
-      on:keydown={handleKeyDown}>{name}</Button
+      on:keydown={handleKeyDown}
+    >
+      <img src={icon} alt={icon} class="w-10 h-10" />
+      {name}</Button
     >
   {/each}
 </div>
