84aa3be701dbd414e4e0d2e140d5c50299010653
anandhuremanan <ananduor@gmail.com>
2024-06-03 10:43:46 +0530

    Spinner Added

---
+++ src/routes/components/Spinner/Spinner.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { Spinner } from "@grampro/svelte-block";
+</script>
+
+<div class="flex gap-4">
+  <Spinner />
+  <Spinner type="stroke" />
+  <Spinner type="circle-bg" />
+  <Spinner type="dot" size="h-4 w-4" />
+</div>
