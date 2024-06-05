fd1d6961175606c9ff70caaa9c0bdd2f7705f966
anandhuremanan <ananduor@gmail.com>
2024-06-05 11:40:31 +0530

    Checkbox and Uploder added

---
+++ src/routes/components/Checkbox/Checkbox.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { CheckBox } from "@grampro/svelte-block";
+
+  let checked: boolean = false;
+</script>
+
+<div class="flex items-center gap-2">
+  <CheckBox bind:checked />
+  <span>Selected Value is: {checked} </span>
+</div>
+
+<div class="mt-2 flex items-center gap-2">
+  <CheckBox checkboxClass="text-red-500" class="outline-none" checked />
+  <span>Checked State</span>
+  <CheckBox checkboxClass="text-red-500" class="outline-none" disabled />
+  <span>Disabled State</span>
+</div>
---
+++ src/routes/components/Uploader/Uploader.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { Uploader } from "@grampro/svelte-block";
+
+  let selectedFiles: any = null;
+</script>
+
+<div class="flex flex-col gap-2">
+  <Uploader bind:selectedFiles />
+  <div class="font-bold text-sm">With Preview and allow multiple</div>
+  <Uploader accept=".png" bind:selectedFiles placeHolder="Select Images(.png)" multiple showPreview />
+</div>
