20e3973f619c7abd2b235b7544d53576b7a36091
anandhuremanan <ananduor@gmail.com>
2024-06-05 14:25:32 +0530

    Updated Support

--- src/routes/UI/TableofContent.svelte
+++ src/routes/UI/TableofContent.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+
   export let contentHtml;
 
   let headings: any[] = [];
+  let path: any;
 
   $: {
     if (contentHtml) {
@@ -19,11 +22,15 @@
       }
     }
   }
+
+  onMount(() => {
+    path = window.location.pathname;
+  });
 </script>
 
 <div class="hidden lg:block md:w-52 py-4 dark:text-white">
   <div class="font-bold">On This Page</div>
-  <ul class="ml-2 mt-2 text-sm">
+  <ul class="ml-2 mt-2 text-sm mb-12">
     {#each headings as heading}
       <li
         class={`m-2 hover:text-blue-400 ${heading.tag === "h4" ? "ml-6" : ""}`}
@@ -32,4 +39,18 @@
       </li>
     {/each}
   </ul>
+
+  <hr />
+
+  <div class="font-bold mt-4 text-xs text-gray-600 flex flex-col gap-2">
+    <a
+      target="_blank"
+      href={`https://github.com/ananduremanan/gbs-svelte-buildingblock-docs/tree/main/src/routes${path}/%2Bpage.svx`}
+      >Edit This Page</a
+    ><a
+      target="_blank"
+      href="https://github.com/ananduremanan/svelte-component-library/issues/new"
+      >Raise an Issue</a
+    >
+  </div>
 </div>
