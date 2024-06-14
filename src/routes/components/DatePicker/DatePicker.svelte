ffb19e68222b462e2b21c4786750b73a9168e8bd
anandhuremanan <ananduor@gmail.com>
2024-06-14 17:04:35 +0530

    Updated DatePicker

--- src/routes/components/DatePicker/DatePicker.svelte
+++ src/routes/components/DatePicker/DatePicker.svelte
@@ -1,5 +1,13 @@
-<script>
+<script lang="ts">
   import { DatePicker } from "@grampro/svelte-block";
+  const today = new Date();
 </script>
 
 <DatePicker />
+
+<div class="mt-4 mb-4">With Min and Max</div>
+
+<DatePicker
+  minDate={new Date(today.getFullYear(), today.getMonth(), 1)}
+  maxDate={new Date()}
+/>
