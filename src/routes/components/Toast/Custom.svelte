b3dd82a29bf90c70aab69ad06bd76b3aba56fbd9
anandhuremanan <ananduor@gmail.com>
2024-06-19 13:57:44 +0530

    Updated Black Hole Exporer and Fixed Some Bugs

--- src/routes/components/Toast/Custom.svelte
+++ src/routes/components/Toast/Custom.svelte
@@ -3,5 +3,5 @@
     <img src="/ai_girl.jpg" alt="caller" class="w-12 h-12 rounded-full" />
     <div>Shreya Wants To Marry You!!!</div>
   </div>
-  <button class="bg-green-400 px-2 py-1 rounded-lg text-white">Call</button>
+  <a class="bg-green-400 px-2 py-1 rounded-lg text-white" href="tel:(505) 842-5662">Call</a>
 </div>
